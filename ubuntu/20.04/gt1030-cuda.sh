393143099c67b16a52dc97ab7efe7b5a746fc483
tymosh <77159957+tymoshh@users.noreply.github.com>
2025-06-07 11:15:32 +0100

    Update gt1030-cuda.sh

--- ubuntu/20.04/gt1030-cuda.sh
+++ ubuntu/20.04/gt1030-cuda.sh
@@ -1,14 +1,13 @@
+#!/bin/bash
+
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
 dpkg -i cuda-keyring_1.1-1_all.deb
 apt update
 apt install -y cuda-toolkit-12-8
-#!/bin/bash
 
 CUDA_VERSION=12.8
 CUDA_PATH="/usr/local/cuda-${CUDA_VERSION}"
 PROFILED_FILE="/etc/profile.d/cuda.sh"
-
-echo "Configuring CUDA ${CUDA_VERSION} environment globally..."
 
 cat <<EOF > "${PROFILED_FILE}"
 export PATH=${CUDA_PATH}/bin:\$PATH
@@ -28,5 +27,3 @@ if ! grep -q "LD_LIBRARY_PATH" /etc/environment; then
     echo "LD_LIBRARY_PATH=${CUDA_PATH}/lib64" >> /etc/environment
     echo "LD_LIBRARY_PATH added to /etc/environment"
 fi
-
-echo "CUDA environment configured system-wide."
