ac7245c33184b4bb07d28efa0b3e3ea3197c5cef
tymosh <77159957+tymoshh@users.noreply.github.com>
2025-06-07 11:03:30 +0100

    Create gt1030.sh

---
+++ ubuntu/20.04/gt1030.sh
@@ -0,0 +1,3 @@
+wget https://us.download.nvidia.com/XFree86/Linux-x86_64/570.153.02/NVIDIA-Linux-x86_64-570.153.02.run
+chmod +x ./NVIDIA-Linux-x86_64-570.153.02.run
+./NVIDIA-Linux-x86_64-570.153.02.run --no-kernel-module
